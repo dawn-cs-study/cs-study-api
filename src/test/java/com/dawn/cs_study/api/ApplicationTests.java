176d43294222309a2a43c9acf04b5ebefc53767c
masiljangajji <xmfpdlsj0508@gmail.com>
2025-08-27 20:17:52 +0900

    init: first commit for init project

---
+++ src/test/java/com/dawn/cs_study/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.cs_study.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
